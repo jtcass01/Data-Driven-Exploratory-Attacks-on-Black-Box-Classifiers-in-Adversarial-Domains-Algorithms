eee70a3ebd20a0e08964bfb6c4704489e0c24caf
Jacob Cassady <jtcass01@louisville.edu>
2018-03-14 09:55:46 -0500

    Creation of NeuralNetworkUtilities for test Neural Network.  Will begin to implement Anchor-Points Exploration algorithm tonight.

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,10 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from initialization import initialize_random
-from forward_propagation import propagate_forward
-from backward_propagation import propagate_backward
-from load_datasets import load_planar_dataset
+from NeuralNetworkUtilities import initialize_random, propagate_forward, propagate_backward
 
 class NeuralNetwork(object):
     def __init__(self, layers_dimensions):
@@ -113,10 +110,31 @@ class NeuralNetwork(object):
         plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
         plt.show()
 
+def load_planar_testset():
+    m = 400  # number of examples
+    N = int(m / 2)  # number of points per class
+    D = 2  # dimensionality
+    X = np.zeros((m, D))  # data matrix where each row is a single example
+    Y = np.zeros((m, 1), dtype='uint8')  # labels vector (0 for red, 1 for blue)
+    a = 4  # maximum ray of the flower
+
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+
+    X = X.T
+    Y = Y.T
+
+    return X, Y
+
+
 if __name__ == '__main__':
     test_network = NeuralNetwork(layers_dimensions=[2,4,4,1])
 
-    X, Y = load_planar_dataset()
+    X, Y = load_planar_testset()
 
     print(X.shape, 'X')
     print(Y.shape, 'Y')
